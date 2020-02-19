56d4582159059f095b5b830d8f7d875ea4cba606
ribo <roibeard.mac.unfraidh@gmail.com>
2020-02-19 12:31:40 +0100

    Import easca-keyboard html as jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
+import {Easca} from './components/easca-keyboard/easca-keyboard'
 function App() {
   return (
-    <div className="App container-fluid">
-      tús
+    <div className="App">
+<Easca />
     </div>
   );
 }
